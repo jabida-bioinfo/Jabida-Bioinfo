ac39c194ff0afa42320504658dbbcd9dd6f05eba
Jabida Sultana <jabida.bioinfo@gmail.com>
2025-11-26 13:44:07 +0530

    tiny tool- dna counter

---
+++ dna-counter.py
@@ -0,0 +1,10 @@
+# My first bioinformatics script – Jabida
+# Counts A, T, G, C in DNA sequence
+
+dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+print("A →", dna.count('A'))
+print("C →", dna.count('C'))
+print("G →", dna.count('G'))
+print("T →", dna.count('T'))
+print("Length →", len(dna))
